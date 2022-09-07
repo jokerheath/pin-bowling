32321f01bb7d93e3a410cf93ffd771a1c0d91c70
jokerheath <53380090+jokerheath@users.noreply.github.com>
2022-09-07 20:36:12 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,64 +7,67 @@ import { Component, OnInit, VERSION } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  result: number;
 
   ngOnInit() {
-    function bowlingScore(frames) {
-      var framesArr = frames.split(' ').map((frame) => [...frame]);
-      console.log(framesArr);
-      var result = 0;
+    this.bowlingScore('24 64 34 34 34 67 56');
+  }
 
-      for (var i = 0; i < framesArr.length; i++) {
-        var current = framesArr[i];
+  bowlingScore(frames) {
+    var framesArr = frames.split(' ').map((frame) => [...frame]);
+    console.log(framesArr);
+    this.result = 0;
 
-        if (current[0] === 'X' && i < 9) {
-          result += 10 + getNextElementsScore(framesArr, i, 2);
-        } else if (current.length === 2 && current[1] === '/' && i < 9) {
-          result += 10 + getNextElementsScore(framesArr, i, 1);
-        } else if (current.length === 3) {
-          result += getThreeRollScore(current);
-        } else {
-          result += getElementScore(current, 0) + getElementScore(current, 1);
-        }
+    for (var i = 0; i < framesArr.length; i++) {
+      var current = framesArr[i];
+
+      if (current[0] === 'X' && i < 9) {
+        this.result += 10 + this.getNextElementsScore(framesArr, i, 2);
+      } else if (current.length === 2 && current[1] === '/' && i < 9) {
+        this.result += 10 + this.getNextElementsScore(framesArr, i, 1);
+      } else if (current.length === 3) {
+        this.result += this.getThreeRollScore(current);
+      } else {
+        this.result +=
+          this.getElementScore(current, 0) + this.getElementScore(current, 1);
       }
-      return result;
     }
+    console.log(this.result);
+    return this.result;
+  }
 
-    function getNextElementsScore(framesArr, index, count) {
-      var result = getElementScore(framesArr[index + 1], 0);
-      var result2 = getElementScore(framesArr[index + 1], 1);
+  getNextElementsScore(framesArr, index, count) {
+    this.result = this.getElementScore(framesArr[index + 1], 0);
+    var result2 = this.getElementScore(framesArr[index + 1], 1);
 
-      if (framesArr[index + 1].length === 1) {
-        return count === 1
-          ? result
-          : result + getElementScore(framesArr[index + 2], 0);
-      }
+    if (framesArr[index + 1].length === 1) {
       return count === 1
-        ? result
-        : result2 === 10 && result !== 10
-        ? result2
-        : result2 + result;
+        ? this.result
+        : this.result + this.getElementScore(framesArr[index + 2], 0);
     }
+    return count === 1
+      ? this.result
+      : result2 === 10 && this.result !== 10
+      ? result2
+      : result2 + this.result;
+  }
 
-    function getElementScore(frame, index) {
-      if (frame[index] === 'X' || frame[index] === '/') {
-        return 10;
-      }
-      return parseInt(frame[index]);
+  getElementScore(frame, index) {
+    if (frame[index] === 'X' || frame[index] === '/') {
+      return 10;
     }
+    return parseInt(frame[index]);
+  }
 
-    function getThreeRollScore(current) {
-      var third = getElementScore(current, 2);
-      var second = getElementScore(current, 1);
-      var first = getElementScore(current, 0);
+  getThreeRollScore(current) {
+    var third = this.getElementScore(current, 2);
+    var second = this.getElementScore(current, 1);
+    var first = this.getElementScore(current, 0);
 
-      return third === 10 && second !== 10
-        ? third + first
-        : second === 10 && first !== 10
-        ? third + second
-        : first + second + third;
-    }
-
-    console.log((bowlingScore('14 36 91 56 46 45 45 45 67 56 45y'), 20));
+    return third === 10 && second !== 10
+      ? third + first
+      : second === 10 && first !== 10
+      ? third + second
+      : first + second + third;
   }
 }
