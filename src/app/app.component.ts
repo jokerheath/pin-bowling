174f7b63d8416b406b56433bd93ca10168a6101a
jokerheath <53380090+jokerheath@users.noreply.github.com>
2022-09-07 22:31:11 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,10 @@ import { Component, OnInit, VERSION } from '@angular/core';
 export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
   result: number;
+  currentScore: number;
 
   ngOnInit() {
-    this.bowlingScore('24 64 34 34 34 67 56');
+    this.bowlingScore('25 46');
   }
 
   bowlingScore(frames) {
@@ -20,11 +21,14 @@ export class AppComponent implements OnInit {
 
     for (var i = 0; i < framesArr.length; i++) {
       var current = framesArr[i];
+      this.currentScore = parseInt(current[0]) + parseInt(current[1]);
+      console.log(typeof this.currentScore);
 
       if (current[0] === 'X' && i < 9) {
         this.result += 10 + this.getNextElementsScore(framesArr, i, 2);
-      } else if (current.length === 2 && current[1] === '/' && i < 9) {
-        this.result += 10 + this.getNextElementsScore(framesArr, i, 1);
+      } else if (current.length === 2 && this.currentScore === 10 && i < 9) {
+        this.result = this.firstElementOfNextFrameForSpare(current);
+        console.log('spareresult ' + this.result);
       } else if (current.length === 3) {
         this.result += this.getThreeRollScore(current);
       } else {
@@ -32,27 +36,41 @@ export class AppComponent implements OnInit {
           this.getElementScore(current, 0) + this.getElementScore(current, 1);
       }
     }
-    console.log(this.result);
+    console.log('finalresult+ ' + typeof this.result);
     return this.result;
   }
 
-  getNextElementsScore(framesArr, index, count) {
-    this.result = this.getElementScore(framesArr[index + 1], 0);
-    var result2 = this.getElementScore(framesArr[index + 1], 1);
+  firstElementOfNextFrameForSpare(framesArr: number): number {
+    //if (framesArr) {
+    console.log('spareelement' + framesArr[0] + framesArr[1]);
+    return 10 + framesArr[0] + framesArr[1];
+    //}
+  }
 
-    if (framesArr[index + 1].length === 1) {
+  getNextElementsScore(framesArr, index, count) {
+    console.log(framesArr, index, count);
+    if (framesArr[index + 1]) {
+      console.log('true');
+      this.result = this.getElementScore(framesArr[index + 1], 0);
+      var result2 = this.getElementScore(framesArr[index + 1], 1);
+      console.log(' this.result+ ' + this.result);
+      console.log('result 2+ ' + result2);
+
+      if (framesArr[index + 1].length === 1) {
+        return count === 1
+          ? this.result
+          : this.result + this.getElementScore(framesArr[index + 2], 0);
+      }
       return count === 1
         ? this.result
-        : this.result + this.getElementScore(framesArr[index + 2], 0);
+        : result2 === 10 && this.result !== 10
+        ? result2
+        : result2 + this.result;
     }
-    return count === 1
-      ? this.result
-      : result2 === 10 && this.result !== 10
-      ? result2
-      : result2 + this.result;
   }
 
   getElementScore(frame, index) {
+    //console.log('frame and index+ ' + frame, index);
     if (frame[index] === 'X' || frame[index] === '/') {
       return 10;
     }
