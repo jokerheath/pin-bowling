585acaac2dc10a23bcb2d3b2edb94c8ad6c1c4c7
jokerheath <53380090+jokerheath@users.noreply.github.com>
2022-09-07 19:25:13 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,64 +7,84 @@ import { Component, OnInit, VERSION } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  rolls: any = [];
+  scores: any;
+  frameIndex: any = [];
 
-  ngOnInit() {
-    function bowlingScore(frames) {
-      var framesArr = frames.split(' ').map((frame) => [...frame]);
-      console.log(framesArr);
-      var result = 0;
+  ngOnInit() {}
 
-      for (var i = 0; i < framesArr.length; i++) {
-        var current = framesArr[i];
+  randomBowl() {
+    if (this.scores == null || 0) {
+      this.scores = Math.floor(Math.random() * 11);
+      console.log('firstScore+ ' + this.scores);
+      this.roll(this.scores);
+      return this.scores;
+    }
+    var rest = 11 - this.scores;
+    this.scores = Math.floor(Math.random() * rest);
+    this.roll(this.scores);
 
-        if (current[0] === 'X' && i < 9) {
-          result += 10 + getNextElementsScore(framesArr, i, 2);
-        } else if (current.length === 2 && current[1] === '/' && i < 9) {
-          result += 10 + getNextElementsScore(framesArr, i, 1);
-        } else if (current.length === 3) {
-          result += getThreeRollScore(current);
-        } else {
-          result += getElementScore(current, 0) + getElementScore(current, 1);
-        }
+    return this.scores;
+  }
+
+  roll(pins) {
+    this.rolls.push(pins); //save number of pins knocked every roll
+    console.log(this.rolls);
+    //console.log(this.score());
+  }
+
+  score() {
+    let score = 0;
+    let rollIndex = 0;
+
+    for (let frameIndex = 0; frameIndex < this.rolls.length; frameIndex++) {
+      if (this.isStrike(rollIndex)) {
+        score += this.strikeBonus(rollIndex);
+        rollIndex++;
+        continue;
       }
-      return result;
-    }
 
-    function getNextElementsScore(framesArr, index, count) {
-      var result = getElementScore(framesArr[index + 1], 0);
-      var result2 = getElementScore(framesArr[index + 1], 1);
-
-      if (framesArr[index + 1].length === 1) {
-        return count === 1
-          ? result
-          : result + getElementScore(framesArr[index + 2], 0);
+      let frameScore = this.sumFrame(rollIndex);
+      if (this.isSpare(frameScore)) {
+        score += this.spareBonus(rollIndex);
+      } else {
+        score += frameScore;
       }
-      return count === 1
-        ? result
-        : result2 === 10 && result !== 10
-        ? result2
-        : result2 + result;
+
+      rollIndex += 2;
     }
+    return score;
+  }
 
-    function getElementScore(frame, index) {
-      if (frame[index] === 'X' || frame[index] === '/') {
-        return 10;
-      }
-      return parseInt(frame[index]);
-    }
+  sumFrame(rollIndex) {
+    console.log(
+      'sumFrame+ ' + this.rolls[rollIndex] + this.rolls[rollIndex + 1]
+    );
 
-    function getThreeRollScore(current) {
-      var third = getElementScore(current, 2);
-      var second = getElementScore(current, 1);
-      var first = getElementScore(current, 0);
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+  }
 
-      return third === 10 && second !== 10
-        ? third + first
-        : second === 10 && first !== 10
-        ? third + second
-        : first + second + third;
-    }
+  spareBonus(rollIndex) {
+    console.log('sparebonus+ ' + 10 + this.rolls[rollIndex + 2]);
+    return 10 + this.rolls[rollIndex + 2];
+  }
+  strikeBonus(rollIndex) {
+    console.log(
+      'strikeBonus+' +
+        10 +
+        this.rolls[rollIndex + 1] +
+        this.rolls[rollIndex + 2]
+    );
+    return 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+  }
 
-    console.log((bowlingScore('14 36 91 56 46 45 45 45 67 56 45y'), 20));
+  isSpare(frameScore) {
+    console.log(frameScore === 10);
+    return frameScore === 10;
+  }
+
+  isStrike(rollIndex) {
+    console.log(this.rolls[rollIndex] === 10);
+    return this.rolls[rollIndex] === 10;
   }
 }
